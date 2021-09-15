6c7bf2953303e98bd769c2a4f94460e222c82955
= <=>
2021-09-15 13:52:23 +0200

    add login page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,40 +1,71 @@
 import * as React from "react";
-import { useForm } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 import styled from "styled-components";
 import tw from "twin.macro";
+import { useAuth } from "../../utils/useAuth";
+import toast, { Toaster } from "react-hot-toast";
+import { useRouter } from "next/router";
 
-const Container = styled.div`
-  ${tw`bg-gray-800 text-blue-100 flex items-center justify-center `}
-  height: 100%;
-  width: 100%;
-  overflow: auto;
-`;
-
-const Form = styled.form`
-  ${tw`flex flex-col max-w-md w-full space-y-8 text-blue-900`}
-`;
-type LoginType = {
-  userName: string;
+export type LoginInputType = {
+  email: string;
   password: string;
 };
+const LoginContainer = styled.div`
+  ${tw`flex justify-center w-full h-full items-center p-4`}
+`;
+const LoginForm = styled.form`
+  ${tw`flex flex-col border rounded-xl min-h-full border-gray-700 w-1/5 bg-gray-600 p-6`}
+`;
+
+const LoginInput = styled.input`
+  ${tw`bg-gray-800 rounded p-2 m-2`}
+`;
+
+const LoginButton = styled.input`
+  ${tw`bg-green-500 rounded p-4 m-2 cursor-pointer`}
+`;
+
 const Login = () => {
-  const { register, handleSubmit } = useForm();
-  const [result, setResult] = React.useState("");
-  const onSubmit = (data: LoginType) =>
-    setResult(JSON.stringify(data, null, 2));
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginInputType>();
+  const { setUserInfo } = useAuth();
+
+  const onSubmit: SubmitHandler<LoginInputType> = async (data) => {
+    const { email, password } = data;
+
+    const respons = await fetch("/api/login", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+    const user = await respons.json();
+    if (user.error) {
+      console.log(user);
+
+      toast.error(user.error);
+      return;
+    }
+    setUserInfo(user);
+    router.push("/");
+  };
+
   return (
-    <Container>
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("userName")} placeholder="Username" />
-        <input
+    <LoginContainer>
+      <Toaster />
+      <LoginForm onSubmit={handleSubmit(onSubmit)}>
+        <LoginInput {...register("email")} placeholder="email" type="text" />
+        <LoginInput
           {...register("password")}
-          placeholder="Password"
-          type="Password"
+          placeholder="password"
+          type="password"
         />
-        <input type="submit" value="Login" />
-        <p>{result}</p>
-      </Form>
-    </Container>
+        <LoginButton type="submit" value="Login" />
+      </LoginForm>
+    </LoginContainer>
   );
 };
 
